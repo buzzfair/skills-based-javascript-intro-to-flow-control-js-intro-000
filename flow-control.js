bbf855be20e4fb44468fbac293fed2bafe082081
Guin White <guin.white@gmail.com>
2019-12-02 18:31:42 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,43 @@
 function basicTeenager(age) {
-
+  if (age > 12 && < 20)
+  return You are a teenager!
 }
 
 function teenager(age) {
-
+  if (age >= 13 && <= 19)
+  console.log ("You are a teenager!")
+  }
+  else if (age < 12 || > 19 ) {
+  console.log ("You are not a teenager!")
+  }
+  else {
+    console.log("I don't know what you are!")
+  }
 }
 
 function ageChecker(age) {
-
+  return (age > 12 && < 20)
+  console.log("You are a teenager!")
+}
+else if (age < 12) {
+  console.log("You are a kid!")
+  }
+  else {
+    return You are a grown up!
+  }
 }
 
 function ternaryTeenager(age) {
-
+return if age > 12 || < 19 ? "You are a teenager" : "You are not a teenager"
+    }
 }
 
 function switchAge(age) {
+  switch(age) {
+    case "13" || "14" || "15" || "16" || "17" || "18" || "19":
+      console.log("You are a teenager");
+      break;
+    default:
+      return You have an age
 
 }
